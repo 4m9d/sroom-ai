2d674fe86a6e3e0fb349f220f8681bac0147b1cd
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2023-07-01 22:13:12 +0900

    [SWM-96] Feat : connect youtube transcript api

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = FastAPI()
 
+DEFAULT_LANGUAGE = 'ko'
 @app.get("/")
-def root():
-    return {"message" : "welcome to sro``om ai server"}
+async def root(video_id: str = '', lang: str = DEFAULT_LANGUAGE):
+    youtube_script = Script()
+    youtube_script.get_script(video_id, lang)
+    return youtube_script.text
+
+
+class Script:
+    def __init__(self):
+        self.raw_script = ''
+        self.text = ''
+
+    def get_script(self, video_id, lang):
+        self.raw_script = YouTubeTranscriptApi.get_transcript(video_id, languages=[lang])
+        self.to_text(self.raw_script)
+
+    def to_text(self, raw_script):
+        for i in range(len(raw_script)):
+            self.text += raw_script[i]['text'] + ' '
