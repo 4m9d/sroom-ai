8f5a2b4c8efdc00368aa1481f50c2646ea6a87d8
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2023-07-17 19:54:25 +0900

    [SWM-180] Refactor : remove recursive logic

--- app/summary/summary.py
+++ app/summary/summary.py
@@ -7,21 +7,33 @@ from app.gpt.gpt import *
 
 def generate_summary(script_text: str):
 
+    text = script_text
     summary_prompt = constants['prompt']['summary']
     chunk_size = 3000
+    summary = ""
 
-    chunks = textwrap.wrap(script_text, chunk_size)
-    chunk_summaries = []
+    while True:
 
-    for chunk in chunks:
-        chunk_summaries.append(request_gpt(chunk + summary_prompt))
+        chunks = textwrap.wrap(text, chunk_size)
+        chunk_summaries = []
 
-    summary = " ".join(chunk_summaries)
+        if len(chunks) > 1:
 
-    if len(chunks) > 3:
-        return generate_summary(summary)
-    elif len(chunks) > 1:
-        summary = request_gpt(summary)
+            for chunk in chunks:
+                chunk_summaries.append(request_gpt(chunk + summary_prompt))
+
+            summary = " ".join(chunk_summaries)
+
+            if len(chunks) > 3:
+                text = summary
+                continue
+
+            summary = request_gpt(summary)
+
+        else:
+            summary = request_gpt(chunks[0])
+
+        break
 
     summary = reformat_summary(summary)
 
