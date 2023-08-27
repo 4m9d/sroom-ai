37a0f156f6c51df771c93f5ee58ed8cfcd1110ba
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2023-08-27 15:44:07 +0900

    Feat : add server config

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import uvicorn
 import yaml
 from fastapi import FastAPI
@@ -5,6 +7,8 @@ from fastapi import FastAPI
 from app.index import *
 
 app = FastAPI()
+
+run_mode = sys.argv[1]
 
 with open('constants.yaml', encoding='UTF-8') as f:
     constants = yaml.load(f, Loader=yaml.FullLoader)
@@ -20,4 +24,7 @@ async def root(video_id: str = '', lang: str = constants['default_language']):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=configs['host']['local'], port=configs['port']['local'])
+    if run_mode == 'local':
+        uvicorn.run(app, host=configs['host']['local'], port=configs['port']['local'])
+    if run_mode == 'server':
+        uvicorn.run(app, host=configs['host']['server'], port=configs['port']['server'])
